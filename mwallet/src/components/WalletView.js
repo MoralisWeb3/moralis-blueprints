9e88ffe9dd8d8bc97bb7ee7f0b3b9fd828f2727e
TzeLoong <tzeloong12@gmail.com>
2023-08-25 07:08:23 +0800

    added sign in functionality with front end rendering

--- mwallet/src/components/WalletView.js
+++ mwallet/src/components/WalletView.js
@@ -12,39 +12,40 @@ import {
 import { LogoutOutlined } from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
 import logo from "../noImg.png";
+import axios from "axios";
 
-const tokens = [
-  {
-    symbol: "ETH",
-    name: "Ethereum",
-    balance: 100000000000,
-    decimals: 18,
-  },
-  {
-    symbol: "LINK",
-    name: "Chainlink",
-    balance: 100000000000,
-    decimals: 18,
-  },
-  {
-    symbol: "UNI",
-    name: "Uniswap",
-    balance: 100000000000,
-    decimals: 18,
-  },
-  {
-    symbol: "MATIC",
-    name: "Polygon",
-    balance: 100000000000,
-    decimals: 18,
-  },
-];
+// const tokens = [
+//   {
+//     symbol: "ETH",
+//     name: "Ethereum",
+//     balance: 100000000000,
+//     decimals: 18,
+//   },
+//   {
+//     symbol: "LINK",
+//     name: "Chainlink",
+//     balance: 100000000000,
+//     decimals: 18,
+//   },
+//   {
+//     symbol: "UNI",
+//     name: "Uniswap",
+//     balance: 100000000000,
+//     decimals: 18,
+//   },
+//   {
+//     symbol: "MATIC",
+//     name: "Polygon",
+//     balance: 100000000000,
+//     decimals: 18,
+//   },
+// ];
 
-const nfts = [
-  "https://nft-preview-media.s3.us-east-1.amazonaws.com/evm/0x1/0xd774557b647330c91bf44cfeab205095f7e6c367/0xfb76f9ef3adabc27d77c615959f9e22dea24ac7d6a10af3458b3481e5f5e0f10/high.png",
-  ,
-  "https://nft-preview-media.s3.us-east-1.amazonaws.com/evm/0x1/0x749f5ddf5ab4c1f26f74560a78300563c34b417d/0x90cae88ffc909feab8e4df76abd0652dee98b7bffab29597d898260d91c20aa1/high.jpeg",
-];
+// const nfts = [
+//   "https://nft-preview-media.s3.us-east-1.amazonaws.com/evm/0x1/0xd774557b647330c91bf44cfeab205095f7e6c367/0xfb76f9ef3adabc27d77c615959f9e22dea24ac7d6a10af3458b3481e5f5e0f10/high.png",
+//   ,
+//   "https://nft-preview-media.s3.us-east-1.amazonaws.com/evm/0x1/0x749f5ddf5ab4c1f26f74560a78300563c34b417d/0x90cae88ffc909feab8e4df76abd0652dee98b7bffab29597d898260d91c20aa1/high.jpeg",
+// ];
 
 // wallet has the public address of the newly created account
 
@@ -56,6 +57,10 @@ function WalletView({
   selectedChain,
 }) {
   const navigate = useNavigate();
+  const [tokens, setTokens] = useState(null);
+  const [nfts, setNfts] = useState(null);
+  const [balance, setBalance] = useState(0);
+  const [fetching, setFetching] = useState(true);
 
   const items = [
     {
@@ -85,7 +90,7 @@ function WalletView({
                     </div>
                   </List.Item>
                 )}
-              ></List>
+              />
             </>
           ) : (
             <>
@@ -132,11 +137,55 @@ function WalletView({
     },
   ];
 
+  async function getAccountTokens() {
+    setFetching(true);
+    const res = await axios.get(`http://localhost:3001/getTokens`, {
+      params: {
+        userAddress: wallet,
+        chain: selectedChain,
+      },
+    });
+
+    const response = res.data;
+
+    if (response.tokens?.length > 0) {
+      setTokens(response.tokens);
+    }
+
+    if (response.nfts?.length > 0) {
+      setNfts(response.nfts);
+    }
+
+    setBalance(response.balance);
+
+    setFetching(false);
+  }
+
   function logout() {
     setSeedPhrase(null);
     setWallet(null);
+    setNfts(null);
+    setTokens(null);
+    setBalance(0);
     navigate("/");
   }
+
+  useEffect(() => {
+    if (!wallet || !selectedChain) return;
+    setNfts(null);
+    setTokens(null);
+    setBalance(0);
+    getAccountTokens();
+  }, []);
+
+  useEffect(() => {
+    if (!wallet) return;
+    setNfts(null);
+    setTokens(null);
+    setBalance(0);
+    getAccountTokens();
+  }, [selectedChain]);
+
   return (
     <>
       <div className="content">
