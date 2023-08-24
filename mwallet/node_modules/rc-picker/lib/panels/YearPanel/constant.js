990d8b94cd62a539d38f4fcd52a20be26037ba40
TzeLoong <tzeloong12@gmail.com>
2023-08-24 15:48:40 +0800

    front-end integration

--- mwallet/node_modules/rc-picker/lib/panels/YearPanel/constant.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.YEAR_DECADE_COUNT = void 0;
-var YEAR_DECADE_COUNT = 10;
-exports.YEAR_DECADE_COUNT = YEAR_DECADE_COUNT;
